653810b042b85bdee6dc7bd49c47c803eaccd7a2
sashi0034 <satoshi3104.nishi@gmail.com>
2022-02-10 13:34:33 +0900

    リファクタリング

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import SquareButton from "./SquareButton"
 
 import './App.css';
 
 
 export default class App extends React.Component 
 {
-
 
     render() {
         return (
@@ -18,49 +18,6 @@ export default class App extends React.Component
 }
 
 
-class SquareButton extends React.Component 
-{
-	constructor()
-	{
-		super();
-	}
-
-	render()
-	{
-		return this.renderSquareMatrix();
-	}
-
-	renderSquareMatrix()
-	{
-		let ret = [];
-		for (let i=0; i<4; i++)
-		{
-			ret.push(
-			<div>
-				{this.renderSquareLine()}
-			</div>);
-		}
-		return(ret);
-	}
-
-	renderSquareLine()
-	{
-		let ret = [];
-		for (let i=0; i<4; i++)
-		{
-			ret.push(<button className="square-button">T</button>);
-		}
-		return(ret);
-	}
-
-	renderSquare()
-	{
-		return(
-			<button className="square-button">T</button>
-		);
-	}
-
-}
 
 
 
---
+++ src/SquareButton.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default class SquareButton extends React.Component 
+{
+	constructor()
+	{
+		super();
+	}
+
+	render()
+	{
+		return this.renderSquareMatrix();
+	}
+
+	renderSquareMatrix()
+	{
+		let ret = [];
+		for (let i=0; i<4; i++)
+		{
+			ret.push(
+			<div>
+				{this.renderSquareLine()}
+			</div>);
+		}
+		return(ret);
+	}
+
+	renderSquareLine()
+	{
+		let ret = [];
+		for (let i=0; i<4; i++)
+		{
+			ret.push(<button className="square-button">T</button>);
+		}
+		return(ret);
+	}
+
+	renderSquare()
+	{
+		return(
+			<button className="square-button">T</button>
+		);
+	}
+
+}
