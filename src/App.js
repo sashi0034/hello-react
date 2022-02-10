aeadb572b05c3d9cf5464a099412e3ff7cfdc05d
sashi0034 <satoshi3104.nishi@gmail.com>
2022-02-10 20:54:25 +0900

    menu内容増やした

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import React from "react";
 import SquareButtonField from "./SquareButtonField"
-import SideBar from "./SideBar";
+import {SideBar, EMenu, menuProps} from "./SideBar";
 
 import './App.css';
 
 
 
 
+function getMenuProp(text)
+{
+    return {
+        text: text,
+    }
+}
+menuProps[EMenu.HEX_DEC] = getMenuProp("Hex to deciminal");
+menuProps[EMenu.MEMO] = getMenuProp("Hex memo");
+menuProps[EMenu.COMP] = getMenuProp("Hex complement");
+
+
 
 export default class App extends React.Component 
 {
+	constructor(props)
+	{
+		super(props);
+		this.state = 
+		{
+			menu: EMenu.HEX_DEC
+		}
+	}
+
+	setMenu(value)
+	{
+		this.setState({menu: value});
+	}
 
     render() {
+		
         return (
 			<div className="base-document">
-				<SideBar/>
+				<SideBar menu={this.state.menu} setMenu={(value) => this.setMenu(value)}/>
 
 				<div className="button-area">
 					<SquareButtonField/>
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,15 +1,28 @@
 import React from "react";
 
-export default class SideBar extends React.Component
-{
 
+export const EMenu = {
+	HEX_DEC: 0,
+	MEMO: 1,
+    COMP: 2,
+}
+
+
+
+export var menuProps = {};
+
+
+
+
+export class SideBar extends React.Component
+{
     render()
     {
-        return (<div className="sideBar">
+        return (<div className="side-bar">
 					<div className="menu-top">16進数謎電卓</div>
-					<SideMenu/>
-                    <SideMenu/>
-                    <SideMenu/>
+					<SideMenu index={EMenu.HEX_DEC} onClick={() => this.props.setMenu(EMenu.HEX_DEC)}/>
+                    <SideMenu index={EMenu.MEMO} onClick={() => this.props.setMenu(EMenu.MEMO)}/>
+                    <SideMenu index={EMenu.COMP} onClick={() => this.props.setMenu(EMenu.COMP)}/>
 				</div>)
     }
 
@@ -17,12 +30,19 @@ export default class SideBar extends React.Component
 
 class SideMenu extends React.Component
 {
+
+    
 	render()
 	{
 		let classLink="menu-link";
 		return (
 			<div className="menu-tag">
-				<a href="#hex-to-deciminal" className={classLink}># Hex to Decminal</a><br/>
+				<a 
+                    href="#hex-to-deciminal"
+                    className={classLink}
+                    onClick={this.props.onClick}>
+                    # {menuProps[this.props.index].text}
+                </a><br/>
 			</div>
 		);
 	}
