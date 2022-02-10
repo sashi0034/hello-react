ebb3485fbf3a938397e860444d3623c8ec682bae
sashi0034 <satoshi3104.nishi@gmail.com>
2022-02-10 13:31:44 +0900

    ボタン部分の仮実装

--- src/App.js
+++ src/App.js
@@ -3,22 +3,65 @@ import React from "react";
 import './App.css';
 
 
-export default class App extends React.Component {
-	squareRender()
-	{
-		return(
-			<button className="square">T</button>
-		);
-	}
+export default class App extends React.Component 
+{
+
 
     render() {
         return (
-			<div>
-				<this.squareRender></this.squareRender>
+			<div className="button-area">
+				<SquareButton/>
 			</div>
+			
 		);
     }
-  }
+}
+
+
+class SquareButton extends React.Component 
+{
+	constructor()
+	{
+		super();
+	}
+
+	render()
+	{
+		return this.renderSquareMatrix();
+	}
+
+	renderSquareMatrix()
+	{
+		let ret = [];
+		for (let i=0; i<4; i++)
+		{
+			ret.push(
+			<div>
+				{this.renderSquareLine()}
+			</div>);
+		}
+		return(ret);
+	}
+
+	renderSquareLine()
+	{
+		let ret = [];
+		for (let i=0; i<4; i++)
+		{
+			ret.push(<button className="square-button">T</button>);
+		}
+		return(ret);
+	}
+
+	renderSquare()
+	{
+		return(
+			<button className="square-button">T</button>
+		);
+	}
+
+}
+
 
 
 
